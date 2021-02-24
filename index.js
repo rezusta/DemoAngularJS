8db9213d91dfff9c1009ee010047d6ffd6d6ff94
Inigo <rez@gmail.com>
2021-02-24 20:45:30 +0100

    Correcciones

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+var path = require('path');
+
 app.use(cors())
 
-app.use(express.static(__dirname));
-
-app.get('/', function(req, res) {
-    res.sendFile('index.html', {root: __dirname })
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
+app.use(express.static(__dirname));
+app.listen(process.env.PORT || 3000);
