117fd6863553ebf470c8d02e94d51eb10a9c34bc
Inigo <rez@gmail.com>
2021-02-24 20:42:10 +0100

    Correcciones

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.PORT || 3000;
 const cors = require('cors')
 app.use(cors())
 
-app.use(express.static('client'))
+app.use(express.static(__dirname));
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
-app.all('*', (req, res) => {
-  res.status(200).sendFile('index.html', { root: __dirname });
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root: __dirname })
 });
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
+    console.log(`App running on port ${port}.`)
 })
